a56000f05267486dd581aeb89c07f0adac54ac9e
Alan <ftcursosap@gmail.com>
2026-02-03 18:04:04 -0600

    Semana 7 - Dia 1

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Conexion con Javascript\n\n");
+//Creacion de un array
+let tareas = [];
+console.log(tareas);
+
+//Array con datos
+let colores = ["rojo", "verde", "azul"];
+console.log(colores);
+
+//Acceder por indice
+console.log(colores[0]);
+console.log(colores[1]);
+
+//Tamaño del array
+console.log(colores.length);
