e790d3e2548a5dfa7cf320fdac06d65384fd9d78
Alan <ftcursosap@gmail.com>
2026-02-10 17:46:43 -0600

    Semana 7 - Dia 4

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 console.log("Conexion con Javascript\n\n");
-//Creacion de un array
-let tareas = ["Estudiar", "Practicar", "Descansar"];
+//Creacion de un objeto
+let tarea = {
+    titulo: "Estudiar JavaScript",
+    completada: false,
+    prioridad: 1
+};
 
-//Bucle for
-for (let i = 0; i < tareas.length; i++) {
-    console.log(tareas[i]);    
+console.log(tarea);
+
+
+//Acceder a propiedades
+console.log(tarea.titulo);
+console.log(tarea.completada);
+
+//Modificar propiedades
+tarea.completada = true;
+tarea.prioridad = 2;
+
+console.log(tarea);
+
+//Agregar nueva propiedad
+tarea.fecha = "2026-02-09";
+console.log(tarea);
+
+//Ejercicios de prueba
+
+let persona = {
+    nombre: "Alan",
+    edad: 29,
+    activo: true
 }
 
-//Bucle ForEach
-tareas.forEach(function (tarea) {
-    console.log(tarea);
-})
+console.log(persona);
+console.log(persona.nombre);
+
+persona.nombre = "Alan Javier";
+console.log(persona);
+
+persona.altura = 1.71;
+console.log(persona);
