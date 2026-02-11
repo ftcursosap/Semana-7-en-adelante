96bc4a635cfd7ef159a377634d3dba523ff4b640
Alan <ftcursosap@gmail.com>
2026-02-11 10:48:38 -0600

    Semana 7 - Dia 5

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 console.log("Conexion con Javascript\n\n");
-//Creacion de un objeto
-let tarea = {
-    titulo: "Estudiar JavaScript",
-    completada: false,
-    prioridad: 1
-};
+//Creacion de un Array de objetos
+let tareas = [
+    {titulo: "Estudiar", completada: false},
+    {titulo: "Practicar", completada: true},
+    {titulo: "Descansar", completada: false}
+];
 
-console.log(tarea);
+console.log(tareas);
 
+//Recorrer array de objetos
+tareas.forEach(function (tarea) {
+    console.log(tarea.titulo);
+    console.log(tarea.completada);
+});
 
-//Acceder a propiedades
-console.log(tarea.titulo);
-console.log(tarea.completada);
+//Acceder a un objeto especifico
+console.log(tareas[0].titulo);
 
-//Modificar propiedades
-tarea.completada = true;
-tarea.prioridad = 2;
-
-console.log(tarea);
-
-//Agregar nueva propiedad
-tarea.fecha = "2026-02-09";
-console.log(tarea);
+//Modificar un objeto dentro del array
+tareas[0].completada = true;
+console.log(tareas);
 
 //Ejercicios de prueba
+//1
+let personas = [
+    {nombre: "Alan", edad: 29},
+    {nombre: "Alma", edad: 31},
+    {nombre: "Emma", edad: 2},
+    {nombre: "Irma", edad: 24}
+]
+console.log(personas);
 
-let persona = {
-    nombre: "Alan",
-    edad: 29,
-    activo: true
-}
+//2
+personas.forEach(function (persona) {
+    console.log(persona.nombre);
+});
 
-console.log(persona);
-console.log(persona.nombre);
+//3
+personas[0].edad = 30;
+console.log(personas);
 
-persona.nombre = "Alan Javier";
-console.log(persona);
-
-persona.altura = 1.71;
-console.log(persona);
+//4
+personas.push({
+    nombre: "Itzel",
+    edad: 16
+});
+console.log(personas);
