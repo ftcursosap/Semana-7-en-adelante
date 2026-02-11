b43b87a2ca8736f319d964cb6f56982b66fe48d5
Alan <ftcursosap@gmail.com>
2026-02-11 17:09:59 -0600

    Semana 8 - Dia 2

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 console.log("Conexion con Javascript\n\n");
-//Semana 8 - Dia 1
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data.body);
-    })
+//Semana 8 - Dia 2
+const boton = document.getElementById("btnCargar");
+const titulo = document.getElementById("titulo");
+const contenido = document.getElementById("contenido");
+const valorId = document.getElementById("valorId");
+
+boton.addEventListener("click", function() {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            titulo.textContent = data.name;
+            contenido.textContent = data.username;
+            valorId.textContent = data.id;
+            console.log(data.name);
+            console.log(data.username);
+            console.log(data.id);
+        });
+});
