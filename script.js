a50201aa8944bde738b70aef1b89852239d2c604
Alan <ftcursosap@gmail.com>
2026-02-11 16:55:02 -0600

    Semana 8 - Dia 1

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
 console.log("Conexion con Javascript\n\n");
-//Semana 7 - Dia 6
-let contador = 0;
-let usuarios = [
-    {nombre: "Alan", edad: 29, activo: true},
-    {nombre: "Alma", edad: 31, activo: true},
-    {nombre: "Emma", edad: 2, activo: false},    
-    {nombre: "Irma", edad: 24, activo: false}  
-];
-console.log(usuarios);
-
-//2
-usuarios.forEach(function (usuario) {
-    console.log(usuario.nombre);
-    if(usuario.activo === true) {
-        console.log(usuario);
-        contador++;
-    }
-});
-console.log("Numero de usuarios activos es: "+contador)
-
-//3
-usuarios[0].edad = 30;
-usuarios[3].activo = true;
-
-usuarios.push({
-    nombre: "Itzel",
-    edad: 16,
-    activo: true
-});
-console.log(usuarios);
-console.log("El numero de usuarios es: "+usuarios.length);
-
-//4
-usuarios.pop();
-console.log(usuarios);
-
-console.log("El numero de usuarios es: "+usuarios.length);
+//Semana 8 - Dia 1
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.body);
+    })
