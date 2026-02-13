4c180c8afc473b518666e5d73823df581d42b4c5
Alan <ftcursosap@gmail.com>
2026-02-13 17:08:32 -0600

    Semana 8 - Dia 4

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 console.log("Conexion con Javascript\n\n");
-//Semana 8 - Dia 3
+//Semana 8 - Dia 4
 const boton = document.getElementById("btnCargar");
 const lista = document.getElementById("lista");
+const estado = document.getElementById("estado");
 
 boton.addEventListener("click", function() {
-    fetch("https://jsonplaceholder.typicode.com/users?_limit=5")
-        .then(function (response) {
+    estado.textContent = "Cargando...";
+    lista.innerHTML = "";
+
+    fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+        .then( function (response) {
+            if(!response.ok){
+                throw new Error("Error en la peticion");
+            }
+
             return response.json();
         })
         .then(function (data) {
-
-
-            lista.innerHTML = "";
+            estado.textContent = "";
 
             data.forEach(function (post) {
                 const item = document.createElement("li");
-                item.textContent = post.name;
+                item.textContent = post.title;
                 lista.appendChild(item);
             });
+        })
+        .catch(function (error) {
+            estado.textContent = "Ocurrio un error al cargar los datos";
+            console.error(error)
         });
 });
